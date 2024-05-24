7029086881aac09c8bea48c19d05dbe68ea65a67
rafaeldiogo <devrafaeldiogo@gmail.com>
2024-05-24 16:28:48 -0300

    feat: add repository route

--- src/router.tsx
+++ src/router.tsx
@@ -4,12 +4,17 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Repository } from "./pages/Repository";
 
 export const Routes = () => {
   const publicRoutes: RouteObject[] = [
     {
       path: "/",
       element: <Home />,
+    },
+    {
+      path: "/repos/:username",
+      element: <Repository />,
     },
   ];
 
