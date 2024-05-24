bb969b478d3b1dd6de34a9ce124d2079252a8320
rafaeldiogo <devrafaeldiogo@gmail.com>
2024-05-24 14:06:54 -0300

    feat: create search component

---
+++ src/components/CardUser/index.tsx
@@ -0,0 +1,35 @@
+import { UserProps } from "../../types/userType";
+import { Link } from "react-router-dom";
+
+const CardUser = ({
+  login,
+  avatar_url,
+  followers,
+  following,
+  name,
+}: UserProps) => {
+  return (
+    <div className="w-[100%] shadow-xl bg-white rounded-xl border-2 border-[#eee] flex flex-col items-center justify-between py-7 px-6 gap-4">
+      <div className="w-[100%] flex items-center gap-4">
+        <img className="size-20 rounded-full" src={avatar_url} alt={login} />
+        <div className="flex flex-col">
+            <h2 className="text-lg font-bold lg:text-xl">{login}</h2>
+            <h3 className="text-sm">{name}</h3>
+        </div>
+      </div>     
+      <div className="w-[100%] flex justify-evenly border-b-2">
+        <div className="flex flex-col items-center">
+          <p className="font-bold">{followers}</p>
+          <p>Seguidores:</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <p className="font-bold">{following}</p>
+          <p>Seguindo:</p>
+        </div>
+      </div>
+      <Link to={`/repos/${login}`}  type="button" className="flex items-center justify-center h-10 w-fit rounded-xl px-6 font-semibold text-base bg-blue-700 text-white">Acessar</Link>
+    </div>
+  );
+};
+
+export default CardUser;
---
+++ src/components/Search/SearchBar.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { KeyboardEvent } from "react";
+
+type SearchProps = {
+  fetchUser: (userName: string) => Promise<void>;
+};
+
+export const SearchBar = ({ fetchUser }: SearchProps) => {
+  const [userName, setUserName] = useState("");
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      fetchUser(userName);
+    }
+  };
+
+  return (
+    <div className="flex items-center bg-white w-[100%] rounded-xl h-10 px-4 shadow-md">
+      <button aria-label="search" onClick={() => fetchUser(userName)}>
+        <FaSearch className="fill-blue-500" />
+      </button>
+      <input
+        className="w-[100%] ml-1 bg-transparent border-none h-[100%] text-xl focus:outline-none"
+        type="text"
+        placeholder="Digite o nome do usuário"
+        name="username"
+        id="username"
+        onChange={(e) => setUserName(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+    </div>
+  );
+};
